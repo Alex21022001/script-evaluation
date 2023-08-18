91ef08454c10124e5392c7c4ce0e6a04ae67049a
Alexey <alexeysitiy@gmail.com>
2023-08-18 17:08:23 +0300

    init

---
+++ src/main/java/com/alexsitiy/script/evaluation/ScriptEvaluationApplication.java
@@ -0,0 +1,13 @@
+package com.alexsitiy.script.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptEvaluationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScriptEvaluationApplication.class, args);
+    }
+
+}

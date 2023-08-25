7ff4429d4b7ce304ee423bf136b9474f0c05fe0b
Alexey <alexeysitiy@gmail.com>
2023-08-25 14:03:08 +0300

    doc: Create documentation for listener package

--- src/main/java/com/alexsitiy/script/evaluation/listener/JSScriptInterruptedListener.java
+++ src/main/java/com/alexsitiy/script/evaluation/listener/JSScriptInterruptedListener.java
@@ -6,9 +6,22 @@ import com.alexsitiy.script.evaluation.model.Status;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+/**
+ * The listener that listens to {@link JSScriptInterruptedEvent}.
+ *
+ * @see Component
+ * @see EventListener
+ */
 @Component
 public class JSScriptInterruptedListener {
 
+    /**
+     *  Changes the {@link Status} of the {@link JSScript} to INTERRUPTED, sets its execution time
+     *  and set obtained errors to the script object when the event occurs.
+     *
+     * @param event the occurred event.
+     * @see com.alexsitiy.script.evaluation.thread.task.ScriptTask
+     * */
     @EventListener
     public void onJSScriptExecution(JSScriptInterruptedEvent event){
         JSScript jsScript = (JSScript) event.getSource();
---
+++ src/main/java/com/alexsitiy/script/evaluation/listener/package-info.java
@@ -0,0 +1,6 @@
+/**
+ *  This package involves listeners which listen to particular
+ *  events {@link org.springframework.context.ApplicationEvent} and change
+ *  {@link com.alexsitiy.script.evaluation.model.JSScript} according to the occurred event.
+ * */
+package com.alexsitiy.script.evaluation.listener;
